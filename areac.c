5fbc7f50ef5d7e2419933bfd7c59f8b9c5633e4a
tusharsharma1029 <102008783+tusharsharma1029@users.noreply.github.com>
2022-03-21 12:55:17 +0530

    Update areac.c

--- areac.c
+++ areac.c
@@ -4,10 +4,11 @@
 int main()
 {
     float radius,area;
+    printf("tushar Sharma\n") ;
     printf("Enter the radius of circle:\n");
     scanf("%f",&radius);
     area = PI*radius*radius;
     printf("Area of circle:%.2f",area);
-printf("tushar Sharma") ;
+
     return 0;
 }
