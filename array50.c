f052bf5881d84270478ee2d1bab74662610ac822
tusharsharma1029 <102008783+tusharsharma1029@users.noreply.github.com>
2022-03-21 11:39:40 +0530

    Update and rename array50.c.txt to array50.c

--- array50.c
+++ array50.c
@@ -3,7 +3,7 @@ int main() {
     int n, i;
     float num[100], sum = 0.0, avg;
 
-    printf("Aatif\n");
+    printf("tushar sharma\n");
     printf("Enter the numbers of elements: ");
     scanf("%d", &n);
 
